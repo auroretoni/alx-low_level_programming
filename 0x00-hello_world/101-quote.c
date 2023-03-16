b5321ba27c0e82cb91ca650539377ef50d6f2b54
auroretoni <auroretoni@gmail.com>
2023-03-16 14:32:31 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program tha prints a line to the standard error
+ * return: 1 (success)
+ */
+int main(void)
+{	
+	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
