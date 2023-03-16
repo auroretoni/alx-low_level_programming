3d94f35e8b24cf062b198c6a16af17b9d571f654
auroretoni <auroretoni@gmail.com>
2023-03-16 16:21:50 -0700

    task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - A program tha prints a line to the standard error
  * return: 1 (success)
  */
+
 int main(void)
 {	
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
