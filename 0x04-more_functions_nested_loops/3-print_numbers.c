abf1b616fd642616b25dc4aff8b4f83590336efc
auroretoni <auroretoni@gmail.com>
2023-03-24 05:00:05 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
- * print_numbers - prints numbers 0-9
- *
- * Return: void
- */
-
-void print_numbers(void);
+ * print_numbers - prints numbers 0 to 9
+ * you can only use _putchar twice
+ * Return: 0
+*/
+void print_numbers(void)
 {
-	int c;
-
-	for (c = '0'; c <= '9'; c++);
-
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+int i;
+i = 0;
+while (i <= 9)
+{
+	_putchar(i + '0');
+	i++;
+}
+_putchar('\n');
 }
