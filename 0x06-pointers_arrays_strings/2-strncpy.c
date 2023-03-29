e4e71e670d18a3322791c4366db4e1edc38cb0ca
auroretoni <auroretoni@gmail.com>
2023-03-29 14:33:04 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int x;
 
-	x = 0;
-	while (src[x] != '\0' && x < n)
-{
-	src[x] = dest[x];
-	x++;
-}
-	while (x < n)
-{
+	for (x = 0; x < n && src[x] != '\0'; x++)
+	dest[x] = src[x];
+	
+	for ( ; x < n; x++)
 	dest[x] = '\0';
-	x++;
-}
 	return (dest);
 }
