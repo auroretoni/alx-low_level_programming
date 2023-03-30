d6df33deeb404f5dd13d909dc173fe94f4ce350a
auroretoni <auroretoni@gmail.com>
2023-03-30 10:14:06 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0, op = 0;
+
+	while (op == 0)
+{
+	if ((*(s1 + x) == '\0') && (*(s2 + x) == '\0'))
+	break;
+	op = *(s1 + x) - *(s2 + x);
+	x++;
+	}
+	return (op);
+}
