8317b4d35bf874bca9b5b7dc351fe9b4f4b06ce1
auroretoni <auroretoni@gmail.com>
2023-04-20 11:00:33 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - prints anything.
@@ -48,3 +50,4 @@ void print_all(const char * const format, ...)
 		} i++;
 	}
 	printf("\n"), va_end(valist);
+}
